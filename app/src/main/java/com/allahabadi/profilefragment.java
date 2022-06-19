235912aa8de00e9691d04982706a0fe1b37f8181
navneethh <navneethhh1@gmail.com>
2022-06-19 15:03:37 +0530

    profile and questionitem ui done

--- app/src/main/java/com/allahabadi/profilefragment.java
+++ app/src/main/java/com/allahabadi/profilefragment.java
@@ -58,7 +58,8 @@ public class profilefragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_profilefragment, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profilefragment, container, false);
+        return rootview;
     }
 }
